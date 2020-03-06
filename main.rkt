c087359c97427f2aaaeb84150ba7fedac197da41
[Fabian] <[ramirezfabian003@gmail.com]>
2020-03-05 23:29:22 -0600

    No habia guardado cambios, ahora si

--- main.rkt
+++ main.rkt
@@ -4,12 +4,53 @@
 (require pict3d)
 (require racket/include)
 (include "Logic/cubeDrawer.rkt")
-(include "Graphics/mainWindow.rkt")
 
 
-(define frame (new frame% [label "Rubik Simulator"] [width 800] [height 800]))
 
-(CanvasCube frame)
+(define mainFrame (new frame%
+                       [label "Rubik Simulator"]
+                       [width 1400]
+                       [height 800]
+                       [x 110]
+                       [y 20]))
+
+(define row
+  (new horizontal-panel%
+       [parent mainFrame]
+       [style '(border)]))
+
+(define leftColumn
+  (new horizontal-panel%
+       [parent row]
+       [style '(border)]))
+
+(define cubeColumn
+  (new horizontal-panel%
+       [parent row]
+       [style '(border)]))
+
+(define leftPanel
+  (new vertical-panel%
+       [parent leftColumn]
+       [style '(border)]
+       [alignment '(left center)]
+       ))
+
+; Make a static text message in the frame
 
 
-(send frame show #t)
+(new message%	 
+   	 	[label "Rubik Simulator"]	 
+   	 	[parent leftPanel]
+                [auto-resize #t]
+   	 	)
+
+(define CanvasCube
+  (new pict3d-canvas%
+                    [parent cubeColumn]
+                    [pict3d (combine cubo
+                                     (light (pos 1 1 1)))]))
+
+
+
+(send mainFrame show #t)
